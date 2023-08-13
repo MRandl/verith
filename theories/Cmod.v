4703e87919fcbb61594b73bec98b6cec91b0321a
Mathis Randl <mathis.randl@epfl.ch>
2023-08-13 17:14:21 +0200

    finished proving that sint63 is a com. ring

--- theories/Cmod.v
+++ theories/Cmod.v
@@ -5,45 +5,119 @@ From Verith Require Import ZDivides.
 
 Open Scope Z.
 
-Lemma cmod_add : forall a b c, cmod (cmod (a + b) wB + c) wB = cmod (a + b + c) wB.
+(* This file describes the properrties of cmod, which is very 
+annoying to unfold as it spreads "wB/2" expressions everywhere.
+The theorems proven here should therefore be used as blackbox
+utilities to avoid dealing with the internals of cmod directly. *)
+
+Lemma wb0 : wB > 0. (* comes up a lot because of div_is_mod *)
+Proof.
+    unfold wB, size. lia.
+Qed.
+
+Theorem cmod_means_mod : forall a b, cmod a wB = cmod b wB <-> a mod wB = b mod wB.
+Proof.
+    split; intros H.
+    - unfold cmod in H.
+        assert (((a + wB / 2) mod wB) = (b + wB / 2) mod wB) by lia.
+        clear H.
+        apply div_is_mod in H0.
+        replace (b + wB / 2 - (a + wB / 2)) with (b - a) in H0 by lia.
+        apply div_is_mod. apply wb0.
+        assumption.
+        apply wb0.
+    - unfold cmod.
+        f_equal.
+        apply div_is_mod. exact wb0.
+        replace (b + wB / 2 - (a + wB / 2)) with (b - a) by lia.
+        apply div_is_mod. exact wb0.
+        assumption.
+Qed.
+
+Theorem cmod_idempotent : forall a, cmod (cmod a wB) wB = cmod a wB.
+Proof.
+    intros.
+    apply cmod_means_mod.
+    unfold cmod.
+    rewrite Zminus_mod_idemp_l.
+    f_equal.
+    lia.
+Qed. 
+
+Lemma cmod_add_l : forall a b, cmod (cmod a wB + b) wB = cmod (a + b) wB.
 Proof.
     intros.
     unfold cmod.
     rewrite <- Z.add_assoc.
     rewrite Z.sub_cancel_r.
-    replace (((a + b + wB / 2) mod wB - wB / 2 + (c + wB / 2))) 
-        with ((a + b + wB / 2) mod wB + c) by lia.
+    replace (((a + wB / 2) mod wB - wB / 2 + (b + wB / 2))) 
+        with ((a + wB / 2) mod wB + b) by lia.
     rewrite Zplus_mod_idemp_l.
     f_equal.
     lia.
 Qed.
 
-Lemma cmod_mult : forall a b c, cmod (cmod (a * b) wB * c) wB = cmod (a * b * c) wB.
+Lemma cmod_add_r : forall a b, cmod (b + cmod a wB) wB = cmod (b + a) wB.
+Proof.
+    intros. 
+    rewrite Z.add_comm.
+    rewrite cmod_add_l.
+    f_equal. 
+    lia.
+Qed.
+
+Lemma cmod_mul_l : forall a b, cmod (cmod a wB * b) wB = cmod (a * b) wB.
 Proof.
     intros.
+    apply cmod_means_mod.
     unfold cmod.
-    apply Z.sub_cancel_r.
-    apply div_is_mod. unfold wB, size. lia.
-    replace (a * b * c + wB / 2 -
-    (((a * b + wB / 2) mod wB - wB / 2) * c + wB / 2)) with 
-    (a * b * c - (((a * b + wB / 2) mod wB - wB / 2) * c)) by lia.
+    apply div_is_mod. apply wb0.
     rewrite Z.mul_sub_distr_r.
     rewrite Z.sub_sub_distr.
     rewrite <- Z.add_sub_swap.
-    replace ((a * b + wB / 2) mod wB * c) with (c * ((a * b + wB / 2) mod wB)) by lia.
-    apply div_is_mod. unfold wB, size. lia. 
+    replace ((a + wB / 2) mod wB * b) 
+        with (b * ((a + wB / 2) mod wB)) by lia.
+    apply div_is_mod. apply wb0.
     rewrite Zmult_mod_idemp_r. 
     f_equal.
+    lia.
+Qed.
+
+Lemma cmod_mul_r : forall a b, cmod (b * cmod a wB) wB = cmod (b * a) wB.
+Proof.
+    intros. 
+    rewrite Z.mul_comm.
+    rewrite cmod_mul_l.
+    f_equal. 
     lia.
 Qed.
 
 Lemma cmod_mul_assoc : forall a b c, cmod (cmod (a * b) wB * c) wB = cmod (a * cmod (b * c) wB) wB.
 Proof.
     intros.
-    rewrite cmod_mult.
+    rewrite cmod_mul_l.
     symmetry.
-    rewrite Z.mul_comm .
-    rewrite cmod_mult.
+    rewrite Z.mul_comm.
+    rewrite cmod_mul_l.
     f_equal.
     lia.
 Qed.
+
+Lemma cmod_mul_add_distr : forall a b c, cmod (cmod (a * b) wB + cmod (a * c) wB) wB = cmod (a * cmod (b + c) wB) wB.
+Proof.
+    intros.
+    rewrite cmod_add_l.
+    rewrite Z.add_comm.
+    rewrite cmod_add_l.
+    replace (a * cmod (b + c) wB) with (cmod (b + c) wB * a) by lia.
+    rewrite cmod_mul_l.
+    f_equal.
+    lia.
+Qed.
+
+    
+    
+
+    
+    
+    
--- theories/Signed63.v
+++ theories/Signed63.v
@@ -25,7 +25,7 @@ Proof.
     do 4 rewrite -> add_spec.
     replace (to_Z a + cmod (to_Z b + to_Z c) wB)%Z 
         with (cmod (to_Z b + to_Z c) wB + to_Z a)%Z by lia.
-    do 2 rewrite cmod_add.
+    do 2 rewrite cmod_add_l.
     do 2 f_equal.
     lia.
 Qed.
@@ -76,8 +76,8 @@ Qed.
 and an inverse operation : it is an abelian group :) 
 
 can we push it to a field ? unfortunately, no. Multiplication in Z/nZ is 
-invertible iff n is prime, which is not the case of wB = 2 ^ (Z.of_nat size). 
-As a consolation prize, we can still make ( SInt63, + ) a commutative ring. *)
+invertible iff n is prime, which is not the case of wB = 2 ^ 63. 
+As a consolation prize, we can still make ( SInt63, +, * ) a commutative ring. *)
 
 
 Theorem mul_comm : forall a b, a * b = b * a.
@@ -101,4 +101,51 @@ Proof.
     rewrite cmod_mul_assoc.
     reflexivity.
 Qed.
+
+Theorem mul_add_distr_l: forall a b c, a * (b + c) = a * b + a * c.
+Proof.
+    intros.
+    rewrite <- of_to_Z.
+    rewrite add_spec.
+    do 2 rewrite mul_spec.
+    rewrite <- of_to_Z at 1.
+    rewrite mul_spec.
+    rewrite add_spec.
+    rewrite cmod_add_l, cmod_add_r.
+    rewrite <- Z.mul_add_distr_l.
+    rewrite cmod_mul_r.
+    reflexivity.
+Qed.
+
+Theorem mul_add_distr_r: forall a b c, (b + c) * a = b * a + c * a.
+Proof.
+    intros.
+    rewrite mul_comm.
+    rewrite mul_add_distr_l.
+    rewrite mul_comm; f_equal; apply mul_comm.
+Qed.
+
+Theorem mul_1_l : forall a, 1 * a = a.
+Proof.
+    intros.
+    rewrite <- of_to_Z at 1.
+    rewrite mul_spec.
+    rewrite Z.mul_1_l.
+    rewrite of_Z_cmod.
+    apply of_to_Z.
+Qed.
+
+Theorem mul_1_r : forall a, a * 1 = a.
+Proof.
+    intros.
+    rewrite mul_comm.
+    apply mul_1_l.
+Qed.
+    
+
+(* Sint63 is an abelian group when associated with addition. It also 
+has a multiplication which is associative, distributes over addition 
+and has a neutral element. The multiplication also happens to be 
+commutative, therefore Sint63 forms a commutative ring! *)
+
     
